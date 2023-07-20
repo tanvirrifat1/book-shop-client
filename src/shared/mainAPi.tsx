ca74636e7f21885ed54ee465f216b1d8fa637dbd
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-07-20 21:17:17 +0600

    complete

--- src/shared/mainAPi.tsx
+++ src/shared/mainAPi.tsx
@@ -1,6 +1,6 @@
 // const main_api = "https://mihanserversss.vercel.app";
 // const main_api = "https://book-server-nu.vercel.app/api/v1";
 
-const main_api = "http://localhost:5000/api/v1";
+const main_api = "https://book-shop-server-tanvirrifat1.vercel.app/api/v1";
 
 export default main_api;
